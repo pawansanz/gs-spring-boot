106ee09a37f8845f1d8db921891d0633e9a8261f
pawansanz <pawansanz@gmail.com>
2020-11-08 12:28:49 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "<h1>Greetings from Spring Boot!</h1>";
+        return "<h1>Greetings from Spring Boott!</h1>";
     }
     
 }
